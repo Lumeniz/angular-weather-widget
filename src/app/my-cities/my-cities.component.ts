6d8193687d0950efcfb7462da4a4a09a48dae10e
Lumeniz <lumen_tim@mail.ru>
2017-09-15 17:30:08 +0300

    Highlight currently selected city

--- src/app/my-cities/my-cities.component.ts
+++ src/app/my-cities/my-cities.component.ts
@@ -5,7 +5,7 @@ import * as Redux from 'redux';
 import { AppState } from "../app.reducer";
 import * as MyCitiesActions from '../my-cities/my-cities.actions';
 import * as WeatherForecastActions from '../weather-forecast/weather-forecast.actions';
-import { getMyCities } from "./my-cities.reducer";
+import { getMyCities, getMyCitiesEntities, getCitiesState } from "./my-cities.reducer";
 
 
 @Component( {
@@ -15,12 +15,14 @@ import { getMyCities } from "./my-cities.reducer";
 } )
 export class MyCitiesComponent implements OnInit {
     myCities: City[];
+    currentCityId: number;
 
     constructor( @Inject( AppStore ) private store: Redux.Store<AppState> ){
 
         //listening store for changes and then update our component
         this.store.subscribe( () =>{
-            if ( this.myCities != getMyCities( this.store.getState() ) )
+            if ( this.myCities != getMyCities( this.store.getState() )
+                || this.currentCityId != getCitiesState( this.store.getState() ).currentCity.id  )
                 this.updateComp();
         } );
         //update component on startup
@@ -35,16 +37,19 @@ export class MyCitiesComponent implements OnInit {
         //get my cities from store
         this.myCities = getMyCities( this.store.getState() ) ;
 
-        console.log( this.myCities );
+        this.currentCityId = getCitiesState( this.store.getState() ).currentCity.id;
     }
 
+    //call actions that will set city as current
     setCurrentCity( city: City ){
         this.store.dispatch( MyCitiesActions.setCurrentCity( city ) );
     }
 
 
     removeMyCity( city_id, event ){
+        //removing city from Store
         this.store.dispatch( MyCitiesActions.removeMyCity( city_id ) );
+        //removing city weather forecast from Store
         this.store.dispatch( WeatherForecastActions.removeForecast( city_id ) );
 
         //prevent click action from other parent elements in view
